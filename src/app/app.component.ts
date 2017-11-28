4fa32ad35553c2ebb1bb77989100495137498a8f
Tope Abegunde <tope.abegunde@interactivesys.com>
2017-11-28 18:19:37 -0500

    My first commit for this repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  menu = 'eat';
+
+  selectMenu(menu: string): void {
+    this.menu = menu;
+  }
 }
